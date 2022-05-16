71eda3526e0dbfa626c6bc767076662084ead88f
Brunoomiguell <103513090+Brunoomiguell@users.noreply.github.com>
2022-05-16 11:06:38 -0300

    Add files via upload

---
+++ laços.js
@@ -0,0 +1,65 @@
+/*let x = 1
+while (x <= 10){
+    console.log ('ola , tudo bem ?')
+}*/
+
+
+// faça um laço que repita o seu nome 3 vezes
+
+///let x = 0
+///while (x <= 3 ){
+   /// console.log('Bruno Miguel')
+    ///x = x + 1
+///}
+
+/// Faça um laço que mostre os valores do contador .
+
+//let x = 20
+//while (x < 20)
+//console.log('20')
+//x = x + 20 
+
+//Faça um laço que mostre os valores pares de 0 a 10
+
+//let x = 0
+//while(x)
+///console.log('14')
+//x = x = + 14
+
+///todo numero divisivel por 3 
+
+///let x = 0
+// while(x)
+ //console.log('14')
+ ///x = x = + 14
+
+     //ativida01   
+
+ //let x = 1
+ //while(x <= 100){
+   //  console.log(x)
+    // x= x +1 
+ //}1
+
+ //atividade02
+
+// let x = 50 
+ //while (x <= 100){
+   //  console.log(x)
+    // x = x +1
+ //}
+  
+ //atividade03
+// let x = 10
+ //while(x >= 1){
+//console.log(x)
+//x = x - 1
+// }
+
+//atividade04
+
+//let x = 0 
+//while (x <= 20){
+    //console.log(x)
+  //  x = x + 2
+//}
